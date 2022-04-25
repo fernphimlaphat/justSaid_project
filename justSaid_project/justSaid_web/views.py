1c858cc027c886448ab57f1cea8c7ff70f43b8c6
Poly29887 <tanapon1325@gmail.com>
2022-04-25 16:06:11 +0700

    hompage, blog-detail

--- justSaid_project/justSaid_web/views.py
+++ justSaid_project/justSaid_web/views.py
@@ -1,6 +1,17 @@
 from django.shortcuts import render
+from django.views.generic import ListView,DetailView
+from .models import Post
 
 # Create your views here.
 
-def home(request):
-    return render(request, 'home.html',{})
+#def home(request):
+#    return render(request, 'home.html',{})
+
+# Home page
+class HomeView(ListView):
+    model = Post
+    template_name = 'home.html'
+
+class BlogDetailView(DetailView):
+    model = Post
+    template_name = 'blog_detail.html'
