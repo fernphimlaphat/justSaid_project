ff4dd526f341eca30722dae1d220265e19faee83
Poly29887 <tanapon1325@gmail.com>
2022-04-26 01:53:20 +0700

    User Authentication All

--- justSaid_project/members/views.py
+++ justSaid_project/members/views.py
@@ -6,7 +6,7 @@ from django.urls import reverse_lazy
 
 class UserRegisterView(generic.CreateView):
     form_class = UserCreationForm
-    template_name = 'register.html'
+    template_name = 'registration/register.html'
     success_url = reverse_lazy('login')
 
 # Create your views here.
