12bc382968a1861fe2482af9577046b766094545
Poly29887 <tanapon1325@gmail.com>
2022-04-25 15:22:00 +0700

    folder commit

--- justSaid_project/justSaid_web/models.py
+++ justSaid_project/justSaid_web/models.py
@@ -3,9 +3,9 @@ from django.contrib.auth.models import User
 
 class Post(models.Model):
     title = models.CharField(max_length=255)
-    auther = models.ForeignKey(User,on_delete=models.CASCADE) #ลบเมื่อuserไม่อยู่
-    body=models.TextField()
+    author = models.ForeignKey(User,on_delete=models.CASCADE) #ลบเมื่อuserไม่อยู่
+    body = models.TextField()
     def __str__(self):
-        return self.title + ' | ' + self.author
+        return self.title + ' | ' + str(self.author)
 
 # Create your models here.
