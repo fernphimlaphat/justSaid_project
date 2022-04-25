05cf55e9ecfc5fb21ea3e6b221762bd88ef94c1d
Poly29887 <tanapon1325@gmail.com>
2022-04-25 23:02:58 +0700

    edit post

--- justSaid_project/justSaid_web/forms.py
+++ justSaid_project/justSaid_web/forms.py
@@ -14,3 +14,14 @@ class PostForm(forms.ModelForm):
             'body': forms.Textarea(attrs={'class':'form-control'}),        
             } 
         
+class EditForm(forms.ModelForm):
+    class Meta:
+        model = Post
+        fields =( 'title','title_tag','body')
+
+        widgets = {
+            'title': forms.TextInput(attrs={'class':'form-control','placeholder':'This is Title Placeholder Stuff'}),
+            'title_tag': forms.TextInput(attrs={'class':'form-control'}),
+            'body': forms.Textarea(attrs={'class':'form-control'}),        
+            } 
+        
--- justSaid_project/justSaid_web/urls.py
+++ justSaid_project/justSaid_web/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path
-from .views import AddPostView, HomeView, BlogDetailView
+from .views import AddPostView, HomeView, BlogDetailView, UpdatePostView
 
 urlpatterns = [
     path('', HomeView.as_view(),name='home' ),
     path('blog/<int:pk>', BlogDetailView.as_view(),name='blog-detail'), # pk=primary key
-    path('add_post/',AddPostView.as_view(),name='add_post')
-    
+    path('add_post/',AddPostView.as_view(),name='add_post'),
+    path('blog/edit/<int:pk>',UpdatePostView.as_view(),name='update_post'),
 ]
